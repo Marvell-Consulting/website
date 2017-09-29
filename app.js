9d96fa2c27132f37c11769af9a9886bcb95d19d9
Ben Marvell <madmarv187@gmail.com>
2017-09-29 11:00:33 +0100

    Make BG logo correct proportions
    Add logging to app

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const winston = require('winston');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(require('churchill')(winston));
 
 app.get('/', function (req, res) {
   res.sendFile('./index.html', {root: __dirname });
 });
 
-app.listen(8080);
+app.listen(8080, () => {winston.log('info', 'server started on 8080')});
