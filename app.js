8fa0e754a5ed6135b7e5d24f41346cf7038d7fe4
Leonard Martin <leonard.martin@gmail.com>
2018-02-26 13:46:28 +0000

    Add "jobs" section
    Involved slightly totally re-engineering the entire thing to use dynamic templates instead of static html.
    
    Also DRY-ed out the css a bit.

--- app.js
+++ app.js
@@ -2,12 +2,48 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const winston = require('winston');
+const consolidate = require('consolidate');
+
+const jobs = require('./jobs');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(require('churchill')(winston));
 
+app.engine('html', consolidate.mustache);
+
+app.set('view engine', 'html');
+app.set('views', path.resolve(__dirname, 'views'));
+
+app.use(require('express-partial-templates')(app));
+
+app.use((req, res, next) => {
+  res.locals.jobs = jobs;
+  next();
+});
+
 app.get('/', function (req, res) {
-  res.sendFile('./index.html', {root: __dirname });
+  res.render('index');
+});
+
+app.get('/jobs/:id', function (req, res, next) {
+  const job = jobs.reduce((found, job) => {
+    console.log(job.id, req.params.id)
+    return found || (job.id === req.params.id ? job : null);
+  }, null);
+  if (!job) {
+    next();
+  }
+  res.render(`jobs/${job.id}`);
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.render('404');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.render('500');
 });
 
 app.listen(8080, () => {winston.log('info', 'server started on 8080')});
