3a36a8ff7065b021efc80f5e8783d38abe50f4f2
Ben Marvell <madmarv187@gmail.com>
2022-03-07 11:30:47 +0000

    Take down the content editor role

--- jobs.js
+++ jobs.js
@@ -3,8 +3,8 @@ module.exports = [
   //   id: 'user-researcher',
   //   title: 'Senior User Researcher - Contract'
   // },
-  {
-    id: 'content-designer',
-    title: 'Content Editor - Contract (outside IR35)'
-  }
+  // {
+  //   id: 'content-designer',
+  //   title: 'Content Editor - Contract (outside IR35)'
+  // }
 ];
