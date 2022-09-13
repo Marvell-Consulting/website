e5bf1eb16217edad22593a27df453d4b8758c2df
Mike Thomas <mikieet6@gmail.com>
2022-09-13 16:38:10 +0100

    Adding the NHS standards directory Beta case study

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get('/sitemap.xml', function(req, res) {
     smStream.write({ url: '/casestudy/ho-live'});
     smStream.write({ url: '/casestudy/nhsx-discovery'});
     smStream.write({ url: '/casestudy/nhsx-alpha'});
+    smStream.write({ url: '/casestudy/nhsx-beta'});
     smStream.write({ url: '/casestudy/modern-slavery'});
     smStream.write({ url: '/privacy'});
     smStream.end()
